7deaa373cf16e685a0742c46caa71f19b03c1903
epicsereno <epicdevteam13@gmail.com>
2026-08-12 15:03:04 -0700

    refactor: remove Next.js files to separate repository superdiscount-nextjs

--- app/robots.js
+++
@@ -1,8 +0,0 @@
-const base = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
-
-export default function robots() {
-  return {
-    rules: [{ userAgent: '*', allow: '/', disallow: ['/cart', '/search', '/api/'] }],
-    sitemap: `${base}/sitemap.xml`,
-  };
-}
--- components/PunchTag.jsx
+++
@@ -1,9 +0,0 @@
-/** Signature element: the markdown sticker. One per product image, nowhere else. */
-export default function PunchTag({ pct, size }) {
-  return (
-    <div className={`punch-tag${size === 'lg' ? ' punch-tag-lg' : ''}`} aria-hidden="true">
-      {pct}%
-      <small>off</small>
-    </div>
-  );
-}
